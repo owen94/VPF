c34db782277c2bc0f797966da3b2a2ed17523aa4
owen94 <lcowen.hn@hotmail.com>
2017-08-25 14:49:22 +0900

    sample from the gaussian rbm

--- gbrbm_norb.py
+++ gbrbm_norb.py
@@ -145,216 +145,271 @@ class DBM(object):
 # np.save('final_train_80*80.npy', final_train)
 
 
-# data = np.load('final_train_80*80.npy')
-# data = preprocessing.scale(data/255)
+data = np.load('../LLD/final_train_80*80.npy')
+data = preprocessing.scale(data)
+# print(data[0])
+# plt.imshow(data[50].reshape(80, 80),cmap='gray')
+# plt.savefig('../LLD/gaussian_dbm/original.eps')
+
+gbrbm = GBRBM(n_visible=6400, n_hidden=4000, learning_rate=0.01, momentum=0.95, use_tqdm=True,
+              sample_visible=True, sigma=1)
+
+errs = gbrbm.fit(data, n_epoches=500, batch_size=20)
+
+
+# #
 #
-# bbrbm = GBRBM(n_visible=6400, n_hidden=4000, learning_rate=0.01, momentum=0.95, use_tqdm=True)
-# errs = bbrbm.fit(data, n_epoches=500, batch_size=20)
-
+# # ##################  Generate training data with Gaussian Bernoulli RMB #####
+# #
+# def feed_gaussian(data, w, bhid):
 #
-
-# ##################  Generate training data with Gaussian Bernoulli RMB #####
+#     activation = sigmoid(np.dot(data, w)+  bhid)
+#     samples = np.random.binomial(n=1,p=activation,size=activation.shape)
+#     return samples
 #
-def feed_gaussian(data, w, bhid):
-
-    activation = sigmoid(np.dot(data, w)+  bhid)
-    samples = np.random.binomial(n=1,p=activation,size=activation.shape)
-    return samples
-
-#################  Train the DBM with the input ############################
-
-def train_gdbm(data, hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch = 200):
-
-
-    #########  load the Gaussian RBM data here    ############
-
-
-    num_rbm = len(hidden_list) -1
-    index = T.lscalar()    # index to a mini batch
-    x1 = T.matrix('x1')
-    x2 = T.matrix('x2')
-    x3 = T.matrix('x3')
-
-    if len(hidden_list) == 4:
-
-        if undirected:
-            path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
-               + '_' + str(hidden_list[3]) + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
-        else:
-            path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
-               + '_' + str(hidden_list[3]) + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
-        dbm = DBM(hidden_list = hidden_list,
-              input1=x1,
-              input2=x2,
-              input3=x3,
-              batch_sz=batch_sz)
-
-    elif len(hidden_list) ==3:
-        if undirected:
-            path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
-               + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
-        else:
-            path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
-               + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
-
-        dbm = DBM(hidden_list = hidden_list,
-              input1=x1,
-              input2=x2,
-              batch_sz=batch_sz)
-
-    if not os.path.exists(path):
-        os.makedirs(path)
-
-    n_train_batches = data.shape[0]//batch_sz
-
-    new_data = []
-
-    for i in range(num_rbm):
-        num_units = hidden_list[i] + hidden_list[i+1]
-        new_data.append(theano.shared(value=np.asarray(np.zeros((data.shape[0],num_units)), dtype=theano.config.floatX),
-                                  name = 'train',borrow = True) )
-
-    cost, updates = dbm.get_cost_update(decay_list=decay,learning_rate=lr)
-
-    if len(hidden_list) == 4:
-        train_func = theano.function([index], cost, updates= updates,
-                                 givens= {
-                                     x1: new_data[0][index * batch_sz: (index + 1) * batch_sz],
-                                     x2: new_data[1][index * batch_sz: (index + 1) * batch_sz],
-                                     x3: new_data[2][index * batch_sz: (index + 1) * batch_sz]
-                                 })
-    elif len(hidden_list) ==3:
-        train_func = theano.function([index], cost, updates= updates,
-                                 givens= {
-                                     x1: new_data[0][index * batch_sz: (index + 1) * batch_sz],
-                                     x2: new_data[1][index * batch_sz: (index + 1) * batch_sz],
-                                 })
-
-    mean_epoch_error = []
-    start_time = timeit.default_timer()
-
-    for n_epoch in range(epoch):
-
-        ## propup to get the trainning data
-
-        W = []
-        b = []
-        for i in range(num_rbm):
-            W.append(dbm.W[i].get_value(borrow = True))
-            b.append(dbm.b[i].get_value(borrow = True))
-
-        samplor = get_samples(hidden_list= hidden_list, W=W, b = b)
-        forward_act, forward_data = samplor.forward_pass(input_data= data)
-
-        if undirected:
-            undirected_data = samplor.undirected_pass(forward_act = forward_act)
-            for j in range(num_rbm):
-                new_data[j].set_value(np.asarray(undirected_data[j], dtype=theano.config.floatX))
-        if not undirected:
-            for j in range(num_rbm):
-                new_data[j].set_value(np.asarray(forward_data[j], dtype=theano.config.floatX))
-
-
-        ## Train the dbm
-        mean_cost = []
-        for batch_index in range(n_train_batches):
-            mean_cost += [train_func(batch_index)]
-        mean_epoch_error += [np.mean(mean_cost)]
-        print('The cost for mpf in epoch %d is %f'% (n_epoch,mean_epoch_error[-1]))
-
-        if int(n_epoch+1) % 10 ==0:
-            filename = path + '/dbm_' + str(n_epoch) + '.pkl'
-            save(filename,dbm)
-
-            W = []
-            b = []
-            for i in range(num_rbm):
-                W.append(dbm.W[i].get_value(borrow = True))
-                b.append(dbm.b[i].get_value(borrow = True))
-
-            w_name = path + '/weight_' + str(n_epoch) + '.npy'
-            b_name = path + '/bias_' + str(n_epoch) + '.npy'
-            np.save(w_name,W)
-            np.save(b_name,b)
-
-    loss_savename = path + '/train_loss.eps'
-    show_loss(savename= loss_savename, epoch_error= mean_epoch_error)
-
-    end_time = timeit.default_timer()
-
-    running_time = (end_time - start_time)
-
-    print ('Training took %f minutes' % (running_time / 60.))
-
-
-################  Generate Samples from the DBM ############################
-
-
-def pre_gaussian_samples(data, hidden_list, W, b):
-
-    ############   Feed forward here #######################
-
-    feed_samplor = get_samples(hidden_list=hidden_list, W=W, b=b)
-    feed_data = feed_samplor.get_mean_activation(input_data= data)
-    feed_mean_activation = np.mean(feed_data, axis=0)
-    num_rbm = len(hidden_list) - 1
-
-    n_chains = 100
-    plot_every = 5
-
-    persistent_vis_chain = np.random.binomial(2, p = feed_mean_activation,size=(n_chains, hidden_list[-1]))
-
-    v_samples = persistent_vis_chain
-
-    for i in range(num_rbm):
-
-        vis_units = hidden_list[num_rbm-i - 1]
-        W_sample = W[num_rbm - i -1 ][:vis_units,vis_units:]
-        b_down = b[num_rbm - i -1 ][:vis_units]
-        b_up = b[num_rbm - i -1 ][vis_units:]
-
-        for j in range(plot_every):
-            downact1 = sigmoid(np.dot(v_samples,W_sample.T) + b_down )
-            down_sample1 = np.random.binomial(n=1, p= downact1)
-            upact1 = sigmoid(np.dot(down_sample1,W_sample)+b_up)
-            v_samples = np.random.binomial(n=1,p=upact1)
-        v_samples = down_sample1
-
-    return downact1, down_sample1
-############### Generate Data from Gaussian RBM ############################
-
-
-def gaussian_samples(pre_samples, W, bvis, bhid, gibbs_steps):
-
-    for i in range(gibbs_steps):
-        bottom_output = (np.dot(pre_samples, W.T) + bvis)
-        top_output = sigmoid(np.dot(bottom_output, W) + bhid)
-        pre_samples = np.random.binomial(n=1, p=top_output,size=top_output.shape)
-
-    return top_output
-
-
-W = np.load('../LLD/gaussian/gaussian_w_499.npy')
-bhid = np.load('../LLD/gaussian/gaussian_bhid_499.npy')
-ori_data = np.load('../LLD/final_train_80*80.npy')
-
-
-binary_data = feed_gaussian(data=ori_data,w=W,bhid=bhid)
-print(binary_data.shape)
-
-hidden_list = [4000, 2048, 1024, 512]
-decay = [0.0001, 0.0001, 0.0001, 0.0001]
-lr = 0.001
-
-train_gdbm(data=binary_data,hidden_list=hidden_list,decay=decay,lr=lr, batch_sz=20, epoch=200)
-
-
-
-
-
-
-
-
+# #################  Train the DBM with the input ############################
+#
+# def train_gdbm(data, hidden_list, decay, lr, undirected = False,  batch_sz = 40, epoch = 200):
+#
+#
+#     #########  load the Gaussian RBM data here    ############
+#
+#
+#     num_rbm = len(hidden_list) -1
+#     index = T.lscalar()    # index to a mini batch
+#     x1 = T.matrix('x1')
+#     x2 = T.matrix('x2')
+#     x3 = T.matrix('x3')
+#
+#     if len(hidden_list) == 4:
+#
+#         if undirected:
+#             path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
+#                + '_' + str(hidden_list[3]) + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
+#         else:
+#             path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
+#                + '_' + str(hidden_list[3]) + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
+#         dbm = DBM(hidden_list = hidden_list,
+#               input1=x1,
+#               input2=x2,
+#               input3=x3,
+#               batch_sz=batch_sz)
+#
+#     elif len(hidden_list) ==3:
+#         if undirected:
+#             path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
+#                + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
+#         else:
+#             path = '../LLD/gaussian_dbm/DBM_' + str(hidden_list[1]) + '_' + str(hidden_list[2]) \
+#                + '/decay_' + str(decay[1]) + '/lr_' + str(lr)
+#
+#         dbm = DBM(hidden_list = hidden_list,
+#               input1=x1,
+#               input2=x2,
+#               batch_sz=batch_sz)
+#
+#     if not os.path.exists(path):
+#         os.makedirs(path)
+#
+#     n_train_batches = data.shape[0]//batch_sz
+#
+#     new_data = []
+#
+#     for i in range(num_rbm):
+#         num_units = hidden_list[i] + hidden_list[i+1]
+#         new_data.append(theano.shared(value=np.asarray(np.zeros((data.shape[0],num_units)), dtype=theano.config.floatX),
+#                                   name = 'train',borrow = True) )
+#
+#     cost, updates = dbm.get_cost_update(decay_list=decay,learning_rate=lr)
+#
+#     if len(hidden_list) == 4:
+#         train_func = theano.function([index], cost, updates= updates,
+#                                  givens= {
+#                                      x1: new_data[0][index * batch_sz: (index + 1) * batch_sz],
+#                                      x2: new_data[1][index * batch_sz: (index + 1) * batch_sz],
+#                                      x3: new_data[2][index * batch_sz: (index + 1) * batch_sz]
+#                                  })
+#     elif len(hidden_list) ==3:
+#         train_func = theano.function([index], cost, updates= updates,
+#                                  givens= {
+#                                      x1: new_data[0][index * batch_sz: (index + 1) * batch_sz],
+#                                      x2: new_data[1][index * batch_sz: (index + 1) * batch_sz],
+#                                  })
+#
+#     mean_epoch_error = []
+#     start_time = timeit.default_timer()
+#
+#     for n_epoch in range(epoch):
+#
+#         ## propup to get the trainning data
+#
+#         W = []
+#         b = []
+#         for i in range(num_rbm):
+#             W.append(dbm.W[i].get_value(borrow = True))
+#             b.append(dbm.b[i].get_value(borrow = True))
+#
+#         samplor = get_samples(hidden_list= hidden_list, W=W, b = b)
+#         forward_act, forward_data = samplor.forward_pass(input_data= data)
+#
+#         if undirected:
+#             undirected_data = samplor.undirected_pass(forward_act = forward_act)
+#             for j in range(num_rbm):
+#                 new_data[j].set_value(np.asarray(undirected_data[j], dtype=theano.config.floatX))
+#         if not undirected:
+#             for j in range(num_rbm):
+#                 new_data[j].set_value(np.asarray(forward_data[j], dtype=theano.config.floatX))
+#
+#
+#         ## Train the dbm
+#         mean_cost = []
+#         for batch_index in range(n_train_batches):
+#             mean_cost += [train_func(batch_index)]
+#         mean_epoch_error += [np.mean(mean_cost)]
+#         print('The cost for mpf in epoch %d is %f'% (n_epoch,mean_epoch_error[-1]))
+#
+#         if int(n_epoch+1) % 10 ==0:
+#             filename = path + '/dbm_' + str(n_epoch) + '.pkl'
+#             save(filename,dbm)
+#
+#             W = []
+#             b = []
+#             for i in range(num_rbm):
+#                 W.append(dbm.W[i].get_value(borrow = True))
+#                 b.append(dbm.b[i].get_value(borrow = True))
+#
+#             w_name = path + '/weight_' + str(n_epoch) + '.npy'
+#             b_name = path + '/bias_' + str(n_epoch) + '.npy'
+#             np.save(w_name,W)
+#             np.save(b_name,b)
+#
+#     loss_savename = path + '/train_loss.eps'
+#     show_loss(savename= loss_savename, epoch_error= mean_epoch_error)
+#
+#     end_time = timeit.default_timer()
+#
+#     running_time = (end_time - start_time)
+#
+#     print ('Training took %f minutes' % (running_time / 60.))
+#
+#
+# ################  Generate Samples from the DBM ############################
+#
+#
+# def pre_gaussian_samples(data, hidden_list, W, b, plot_every = 5, num_samples = 100):
+#
+#     ############   Feed forward here #######################
+#
+#     feed_samplor = get_samples(hidden_list=hidden_list, W=W, b=b)
+#     feed_data = feed_samplor.get_mean_activation(input_data= data)
+#     feed_mean_activation = np.mean(feed_data, axis=0)
+#     num_rbm = len(hidden_list) - 1
+#
+#     n_chains = num_samples
+#     plot_every = plot_every
+#
+#     persistent_vis_chain = np.random.binomial(2, p = feed_mean_activation,size=(n_chains, hidden_list[-1]))
+#
+#     v_samples = persistent_vis_chain
+#
+#     for i in range(num_rbm):
+#
+#         vis_units = hidden_list[num_rbm-i - 1]
+#         W_sample = W[num_rbm - i -1 ][:vis_units,vis_units:]
+#         b_down = b[num_rbm - i -1 ][:vis_units]
+#         b_up = b[num_rbm - i -1 ][vis_units:]
+#
+#         for j in range(plot_every):
+#             downact1 = sigmoid(np.dot(v_samples,W_sample.T) + b_down )
+#             down_sample1 = np.random.binomial(n=1, p= downact1)
+#             upact1 = sigmoid(np.dot(down_sample1,W_sample)+b_up)
+#             v_samples = np.random.binomial(n=1,p=upact1)
+#         v_samples = down_sample1
+#
+#     return downact1, down_sample1
+# ############### Generate Data from Gaussian RBM ############################
+#
+#
+# def gaussian_samples(pre_samples, W, bvis, bhid, gibbs_steps):
+#
+#     for i in range(gibbs_steps):
+#         bottom_output = (np.dot(pre_samples, W.T) + bvis)
+#         top_output = sigmoid(np.dot(bottom_output, W) + bhid)
+#         pre_samples = np.random.binomial(n=1, p=top_output,size=top_output.shape)
+#
+#     return bottom_output
+#
+#
+# def _run():
+#     W = np.load('../LLD/gaussian/gaussian_w_499.npy')
+#     bhid = np.load('../LLD/gaussian/gaussian_bhid_499.npy')
+#     ori_data = np.load('../LLD/final_train_80*80.npy')
+#
+#
+#     binary_data = feed_gaussian(data=ori_data,w=W,bhid=bhid)
+#     print(binary_data.shape)
+#
+#     hidden_list = [4000, 2048, 1024, 512]
+#     decay = [0.0001, 0.0001, 0.0001, 0.0001]
+#     lr = 0.001
+#
+#     train_gdbm(data=binary_data,hidden_list=hidden_list,decay=decay,lr=lr, batch_sz=20, epoch=200)
+#
+# def _sample_dbm():
+#     gW = np.load('../LLD/gaussian/gaussian_w_499.npy')
+#     gbhid = np.load('../LLD/gaussian/gaussian_bhid_499.npy')
+#     ori_data = np.load('../LLD/final_train_80*80.npy')
+#     gb_vis = np.load('../LLD/gaussian/gaussian_bvis_499.npy')
+#
+#     dW = np.load('../LLD/gaussian_dbm/weight_39.npy')
+#     db = np.load('../LLD/gaussian_dbm/bias_39.npy')
+#
+#     hidden_list = [4000, 2048, 1024, 512]
+#
+#     binary_data = feed_gaussian(data=ori_data, w=gW,bhid=gbhid)
+#
+#     print(binary_data.shape)
+#
+#     downact1, pre_samples = pre_gaussian_samples(binary_data,hidden_list,dW,db, plot_every=5, num_samples=10)
+#
+#     savepath_dbm = '../LLD/gaussian_dbm/dbm_outut.npy'
+#     np.save(savepath_dbm, pre_samples)
+#
+#     return savepath_dbm
+#
+# def _samples_norb(savepath_dbm, gibbs_steps = 100):
+#
+#     pre_samples = np.load(savepath_dbm)
+#     gW = np.load('../LLD/gaussian/gaussian_w_499.npy')
+#     gbhid = np.load('../LLD/gaussian/gaussian_bhid_499.npy')
+#     gb_vis = np.load('../LLD/gaussian/gaussian_bvis_499.npy')
+#
+#     ori_data = np.load('../LLD/final_train_80*80.npy')
+#     pre_samples = feed_gaussian(data=ori_data, w=gW,bhid=gbhid)[:1,:]
+#     print(pre_samples.shape)
+#
+#     g_samples = gaussian_samples(pre_samples,gW,gb_vis,gbhid,gibbs_steps=gibbs_steps)
+#
+#     savepath = '../LLD/gaussian_dbm/generated_samples.npy'
+#     np.save(savepath, g_samples)
+#     plt.imshow(g_samples[0].reshape(80, 80),cmap='gray')
+#     plt.show()
+#
+#
+# #_sample_dbm()
+# #
+# savepath_dbm = '../LLD/gaussian_dbm/dbm_outut.npy'
+#
+# _samples_norb(savepath_dbm, gibbs_steps=1000)
+#
+# savepath = '../LLD/gaussian_dbm/generated_samples.npy'
+#
+# g_samples = np.load(savepath)
+# #g_samples = preprocessing.scale(g_samples)
+# print(np.min(g_samples[0]))
+# print(g_samples.shape)
+# plt.imshow((g_samples[0].reshape(80, 80)),cmap='gray')
+# plt.savefig('../LLD/gaussian_dbm/samples.eps')
     ##  run the previous functions all together, plot the final samples
 
 
